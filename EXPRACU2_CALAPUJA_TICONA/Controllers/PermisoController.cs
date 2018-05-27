65ec3948bc11680c69b2c16887306a06185b5c91
SERGIO\Zeze <sticona6@gmail.com>
2018-05-27 12:45:04 -0500

    Modificando

---
+++ EXPRACU2_CALAPUJA_TICONA/Controllers/PermisoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using EXPRACU2_CALAPUJA_TICONA.Models;
+
+namespace EXPRACU2_CALAPUJA_TICONA.Controllers
+{
+    public class PermisoController : Controller
+    {
+        // GET: Permisos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
