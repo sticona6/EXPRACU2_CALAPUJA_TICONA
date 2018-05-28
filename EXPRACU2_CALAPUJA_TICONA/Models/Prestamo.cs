3ccb857d4c0f2e4a99c558edf0cf0b1d450d5367
Samuel <30917753+raseth2@users.noreply.github.com>
2018-05-27 20:25:55 -0500

    prestamo

--- EXPRACU2_CALAPUJA_TICONA/Models/Prestamo.cs
+++ EXPRACU2_CALAPUJA_TICONA/Models/Prestamo.cs
@@ -6,6 +6,9 @@ namespace EXPRACU2_CALAPUJA_TICONA.Models
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
+    using System.Linq;
+    using System.Data.Entity;
+
     [Table("Prestamo")]
     public partial class Prestamo
     {
@@ -31,5 +34,124 @@ namespace EXPRACU2_CALAPUJA_TICONA.Models
         public virtual Personal Personal { get; set; }
 
         public virtual Tipo_Pago_Prestamo Tipo_Pago_Prestamo { get; set; }
+
+
+
+
+        //crear el metodo listado
+        public List<Prestamo> Listar()//retornar una collection
+        {
+            var prestamo = new List<Prestamo>();
+
+            try
+            {
+                //conexion con la mfuente de datos
+                using (var db = new Model_Personal())
+                {
+                    prestamo = db.Prestamo.Include("Personal").Include("Tipo_Pago_Prestamo").ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return prestamo;
+        }
+
+        //mnetodo obtener
+        public Prestamo Obtener(int id)//retorna un solo objeto
+        {
+            var prestamo = new Prestamo();
+
+            try
+            {
+                //conexion con la mfuente de datos
+                using (var db = new Model_Personal())
+                {
+                    prestamo = db.Prestamo.Include("Personal").Include("Tipo_Pago_Prestamo")
+                        .Where(x => x.id_prestamo == id)
+                        .SingleOrDefault();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return prestamo;
+        }
+
+
+        //crear el metodo Buscar
+        public List<Prestamo> Buscar(string criterio)//retornar una collection
+        {
+            var prestamo = new List<Prestamo>();
+            string estadousu = "";
+            if (criterio.Equals("Activo")) estadousu = "A";
+            if (criterio == "Inactivo") estadousu = "I";
+
+            try
+            {
+                //conexion con la mfuente de datos
+                using (var db = new Model_Personal())
+                {
+
+                    prestamo = db.Prestamo.Include("Personal")
+                        .Where(x => x.Personal.apellidos_personal.Contains(criterio))
+                                    .ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return prestamo;
+        }
+
+        //metodo guardar
+        public void Guardar()
+        {
+            try
+            {
+                using (var db = new Model_Personal())
+                {
+                    if (this.id_prestamo > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Entry(this).State = EntityState.Added;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        //metodo eliminar
+        public void Eliminar()
+        {
+            try
+            {
+                using (var db = new Model_Personal())
+                {
+                    db.Entry(this).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+
     }
 }
--- EXPRACU2_CALAPUJA_TICONA/Models/Tipo_Pago_Prestamo.cs
+++ EXPRACU2_CALAPUJA_TICONA/Models/Tipo_Pago_Prestamo.cs
@@ -5,6 +5,8 @@ namespace EXPRACU2_CALAPUJA_TICONA.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
+    using System.Data.Entity;
 
     public partial class Tipo_Pago_Prestamo
     {
@@ -29,5 +31,120 @@ namespace EXPRACU2_CALAPUJA_TICONA.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Prestamo> Prestamo { get; set; }
+
+
+
+
+
+        //crear el metodo listado
+        public List<Tipo_Pago_Prestamo> Listar()//retornar una collection
+        {
+            var tipo_pago_Prestamo = new List<Tipo_Pago_Prestamo>();
+
+            try
+            {
+                //conexion con la mfuente de datos
+                using (var db = new Model_Personal())
+                {
+                    tipo_pago_Prestamo = db.Tipo_Pago_Prestamo.ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return tipo_pago_Prestamo;
+        }
+
+        //mnetodo obtener
+        public Tipo_Pago_Prestamo Obtener(int id)//retorna un solo objeto
+        {
+            var tipo_pago_prestamo = new Tipo_Pago_Prestamo();
+
+            try
+            {
+                //conexion con la mfuente de datos
+                using (var db = new Model_Personal())
+                {
+                    tipo_pago_prestamo = db.Tipo_Pago_Prestamo
+                        .Where(x => x.id_tipo_pago_prestamo == id)
+                        .SingleOrDefault();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return tipo_pago_prestamo;
+        }
+
+
+        //crear el metodo Buscar
+        public List<Tipo_Pago_Prestamo> Buscar(string criterio)//retornar una collection
+        {
+            var tipo_pago_prestamo = new List<Tipo_Pago_Prestamo>();
+
+            try
+            {
+                //conexion con la mfuente de datos
+                using (var db = new Model_Personal())
+                {
+
+                    tipo_pago_prestamo = db.Tipo_Pago_Prestamo
+                        .Where(x => x.nombre_tipo_pago.Contains(criterio))
+                                    .ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return tipo_pago_prestamo;
+        }
+
+        //metodo guardar
+        public void Guardar()
+        {
+            try
+            {
+                using (var db = new Model_Personal())
+                {
+                    if (this.id_tipo_pago_prestamo > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Entry(this).State = EntityState.Added;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        //metodo eliminar
+        public void Eliminar()
+        {
+            try
+            {
+                using (var db = new Model_Personal())
+                {
+                    db.Entry(this).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
     }
 }
